aab4af467c397be75eaa3c7e5f25bb0bd3c948e9
HSTAvailDev <availdev@hst.ie>
2018-08-29 10:12:21 +0530

    Minor fixings done

--- app/src/main/java/com/app/imageanim/MainActivity.java
+++ app/src/main/java/com/app/imageanim/MainActivity.java
@@ -1,19 +1,27 @@
 package com.app.imageanim;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,14 +30,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     HorizontalScrollView v_scroll;
     Button btn_left, btn_right;
-    int pos = 40;
-    int temppos = 125;
+    int pos = 0;
+    int temppos = 0;
     int initialpos = 0;
     RecyclerView rv_recycler;
     UserApapter adapter;
     List<Users> user = new ArrayList<>();
     Context mContext;
+    boolean isFirsttime = true;
+    ImageView iv_image;
+    String imageurl = "https://www.isometrix.com/wp-content/uploads/2017/03/featured-images-about.jpg";
+    int screenwidth = 0, imagewidth = 0;
+    int scroolby_val = 0;
 
+    @SuppressLint("ClickableViewAccessibility")
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +51,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         mContext = this;
 
+        iv_image = findViewById(R.id.iv_image);
         v_scroll = findViewById(R.id.v_scroll);
         btn_left = findViewById(R.id.btn_left);
         btn_right = findViewById(R.id.btn_right);
         rv_recycler = findViewById(R.id.rv_recycler);
         btn_left.setOnClickListener(this);
         btn_right.setOnClickListener(this);
-
 
         v_scroll.setOnTouchListener(new View.OnTouchListener() {
 
@@ -53,46 +67,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        filllist();
+        getScreenSize();
+        setupimage();
+        getleftscroll();
+        // when scrolled to the leftmost position the variables are initialized
         v_scroll.setOnScrollChangeListener(new View.OnScrollChangeListener() {
 
             @Override
             public void onScrollChange(View view, int i, int i1, int i2, int i3) {
-                Log.d("Positioni", "" + i);
-                Log.d("Positioni1", "" + i1);
-                Log.d("Positioni2", "" + i2);
-                Log.d("Positioni3", "" + i3);
                 initialpos = i;
                 if (i == 0) {
-                    temppos = 200;
-                    pos = 40;
-                    btn_left.setEnabled(false);
+                    temppos = 0;
                 } else {
-                    btn_left.setEnabled(true);
                 }
             }
         });
 
-
+        ///not able to understand how to handle the values
+        //handling the scroll
         rv_recycler.addOnScrollListener(new RecyclerView.OnScrollListener() {
 
             @Override
             public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
                 super.onScrolled(recyclerView, dx, dy);
+                getleftscroll();
                 if (dy > 0) {
-                    // Scrolling up
+                    temppos = temppos + scroolby_val;
+                    v_scroll.scrollTo(temppos, 0);
                     Log.d("Scroll", "ScrollUp");
-
-                    if (initialpos != 0) {
-                        temppos = temppos - pos--;
-                        v_scroll.scrollTo(temppos, 0);
-                        Log.d("temppos", "" + temppos);
-                    }
-                } else {
-                    temppos = temppos + pos++;
-                    v_scroll.scrollTo(temppos, -temppos);
                     Log.d("temppos", "" + temppos);
-                    // Scrolling down
-                    Log.d("Scroll", "ScrollDown");
+                } else {
+                    if (initialpos != 0) {
+                        temppos = temppos - scroolby_val;
+                        v_scroll.scrollTo(temppos, 0);
+                        Log.d("temppos1", "" + temppos);
+                        Log.d("Scroll", "ScrollDown");
+                    }
                 }
             }
 
@@ -111,12 +122,51 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
 
 
-        filllist();
+    }
+
+    private void getScreenSize() {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+        Log.d("ScreenWidth", "" + width);
+        screenwidth = width;
+    }
+
+    private void setupimage() {
+        Glide.with(mContext)
+                .asBitmap()
+                .load(imageurl)
+                .into(new SimpleTarget<Bitmap>() {
+                    @Override
+                    public void onResourceReady(Bitmap bitmap, Transition<? super Bitmap> transition) {
+                        int w = bitmap.getWidth();
+                        int h = bitmap.getHeight();
+                        Log.d("ImageWitdh", "" + w);
+                        iv_image.setImageBitmap(bitmap);
+                        imagewidth = w;
+                    }
+                });
 
 
     }
 
+    private void getleftscroll() {
+        imagewidth = 2000;
+        if (imagewidth > screenwidth) {
+            int leftoutscreen = imagewidth - screenwidth;
+            scroolby_val = leftoutscreen / user.size();
+            if (scroolby_val == 0) {
+                scroolby_val = 1;
+            }
+            // temppos = 0;
+            Log.d("scroolby_val", "" + scroolby_val);
+        }
 
+    }
+
+
+    ///button is just for demo purpose///
     @Override
     public void onClick(View v) {
         if (v == btn_left) {
@@ -135,31 +185,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private void filllist() {
         user.clear();
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i <= 300; i++) {
             user.add(new Users(String.valueOf(i + 1), "name " + i));
         }
-
         inflateadapter();
     }
 
     private void inflateadapter() {
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-
-
         adapter = new UserApapter(this, R.layout.row_users, user, this);
         rv_recycler.setLayoutManager(mLayoutManager);
-        /*rv_recycler.setLayoutManager(new LinearLayoutManager(this));
-        RecyclerViewMargin decoration = new RecyclerViewMargin(15, user.size());
-        rv_recycler.addItemDecoration(decoration);*/
         rv_recycler.setAdapter(adapter);
-        // mLayoutManager.getChildCount();
-
     }
 
 
     @Override
     public void Currentposition(int position) {
         Log.d("Currentposition", "" + position);
+        if (position == user.size() - 1) {
+            if (scroolby_val == 1) {
+                Log.d("scroolby_val", "" + scroolby_val);
+                Log.d("Inside", "Inside");
+                // v_scroll.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
+                // temppos = 1;
+            }
+        } else if (position == 0) {
+            temppos = 0;
+        }
+        pos = position;
     }
 
     @Override
